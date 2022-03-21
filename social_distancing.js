a8f2d801d26fbf13dce86563a5f1942b08bb23b6
America Ocampo <americavocampo@gmail.com>
2022-03-21 11:48:18 -0700

    commit

---
+++ social_distancing.js
@@ -0,0 +1,85 @@
+/* 
+Given an array of ints representing a line of people where the space between
+indexes is 1 foot, with 0 meaning no one is there and 1 meaning someone is in
+that space,
+return whether or not there is at least 6 feet separating every person.
+Bonus: O(n) linear time (avoid nested loops that cause re-visiting indexes).
+*/
+
+const queue1 = [0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1];
+const expected1 = false;
+
+const queue2 = [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1];
+const expected2 = true;
+
+const queue3 = [1, 0, 0, 0, 0, 0, 0, 0, 1];
+const expected3 = true;
+
+const queue4 = [];
+const expected4 = true;
+
+const queue5 = [1];
+const expected7 = true;
+
+/**
+ * Determines whether each occupied space in the line of people is separated by
+ * 6 empty spaces.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {Array<0|1>} queue
+ * @returns {Boolean}
+ */
+function socialDistancingEnforcer(queue) {
+    var count = 0
+    var ifTrue = false
+    if (queue.length<= 1){
+        return true
+    }
+    for(var i = 0;i<queue.length; i++) {
+        if (queue[i] ==0){
+            count++;
+        }
+        else if (count >= 6){
+            count = 0;
+            ifTrue = true;
+        }
+        else if (count < 6){
+            count = 0;
+            ifTrue = false;
+        }
+    }
+    return ifTrue
+}
+console.log(socialDistancingEnforcer(queue1));
+
+
+// ======================================================================================
+
+
+
+
+/* 
+  Balance Index
+  Here, a balance point is ON an index, not between indices.
+  Return the balance index where sums are equal on either side
+  (exclude its own value).
+  
+  Return -1 if none exist.
+  
+*/
+
+const nums1 = [-2, 5, 7, 0, 3];
+const expected5 = 2;
+
+const nums2 = [9, 9];
+const expected6 = -1;
+
+/**
+ * Finds the balance index in the given array where the sum to the left of the
+ *    index is equal to the sum to the right of the index.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {Array<number>} nums
+ * @returns {number} The balance index or -1 if there is none.
+ */
+//
